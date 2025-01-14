3d612dd54ccc20ef8c730799e8b647d64b45e003
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2025-01-14 17:51:49 +0530

    Remove fallback text

--- packages/block-library/src/details/save.js
+++ packages/block-library/src/details/save.js
@@ -2,19 +2,15 @@
  * WordPress dependencies
  */
 import { RichText, useBlockProps, InnerBlocks } from '@wordpress/block-editor';
-import { __ } from '@wordpress/i18n';
 
 export default function save( { attributes } ) {
 	const { showContent } = attributes;
-	const summary = attributes.summary?.toString().trim()
-		? attributes.summary
-		: __( 'Details' );
 	const blockProps = useBlockProps.save();
 
 	return (
 		<details { ...blockProps } open={ showContent }>
 			<summary>
-				<RichText.Content value={ summary } />
+				<RichText.Content value={ attributes.summary } />
 			</summary>
 			<InnerBlocks.Content />
 		</details>
